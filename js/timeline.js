562039de8d3677b11bd3c25d1057f96a8f7dd00b
Emad Iqbal <112495807+emad-malik@users.noreply.github.com>
2024-11-24 19:44:06 +0500

    integration pt2

--- js/timeline.js
+++ js/timeline.js
@@ -7,7 +7,7 @@ const height = 400 - margins.top - margins.bottom;
 // Create SVG for timeline visualization
 const timelineSVG = d3.select("#timeline-container")
     .append("svg")
-    .attr("width", width + margins.left + margins.right) 
+    .attr("width", width + margins.left + margins.right)
     .attr("height", height + margins.top + margins.bottom)
     .append("g")
     .attr("transform", `translate(${margins.left},${margins.top})`);
@@ -15,7 +15,7 @@ const timelineSVG = d3.select("#timeline-container")
 const g = timelineSVG.append("g")
     .attr("transform", `translate(${margins.left},${margins.top})`);
 
-
+// Scales
 const xScale = d3.scaleLinear().range([0, width]);
 const yScale = d3.scaleLinear().range([height, 0]);
 const sizeScale = d3.scaleLinear().range([5, 20]);
@@ -23,11 +23,14 @@ const color_Scale = d3.scaleOrdinal()
     .domain(["AMR", "EUR", "EMR", "AFR", "SEA", "WPR"])
     .range(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"]);
 
+// Axis
 const xAxis = g.append("g").attr("transform", `translate(0,${height})`);
 const yAxis = g.append("g");
 
+// Tooltip
 const tooltip = d3.select("#timeline_tooltip");
 
+// Animation control
 let yearIndex = 0;
 let isPaused = false;
 
@@ -43,17 +46,29 @@ d3.csv("data/preprocessed_data_tb.csv").then(data => {
     const minYear = d3.min(years);
     const maxYear = d3.max(years);
 
-    d3.select("#year-slider").attr("min", minYear).attr("max", maxYear);
+    // Populate the year dropdown dynamically
+    const yearSelect = d3.select("#year-select");
+    yearSelect.selectAll("option")
+        .data(years)
+        .enter()
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
 
+    yearSelect.property("value", minYear);
+
+    // Set axis domains
     xScale.domain([0, 500]);
     yScale.domain([0, d3.max(data, d => d.mortality)]);
     sizeScale.domain([0, d3.max(data, d => d.population)]);
 
+    // Render axis
     xAxis.call(d3.axisBottom(xScale));
     yAxis.call(d3.axisLeft(yScale));
 
-    let selectedRegion = "All";
+    let selectedRegion = "All"; // default region filter
 
+    // Update function for the visualization
     const update = year => {
         let filteredData = data.filter(d => d.year === year && d.mortality >= 10);
         if (selectedRegion !== "All") {
@@ -74,6 +89,7 @@ d3.csv("data/preprocessed_data_tb.csv").then(data => {
                     .html(`
                         <b>Region:</b> ${d.g_whoregion}<br>
                         <b>Country:</b> ${d.country}<br>
+                        <b>Year:</b> ${d.year}<br>
                         <b>Incidence:</b> ${d.incidence}<br>
                         <b>Mortality:</b> ${d.mortality}<br>
                         <b>Population:</b> ${d.population.toLocaleString()}
@@ -97,32 +113,31 @@ d3.csv("data/preprocessed_data_tb.csv").then(data => {
         circles.exit().remove();
     };
 
-    d3.select("#region-filter").on("change", function () {
+    // Region filter change handler
+    d3.select("#region-select").on("change", function () {
         selectedRegion = this.value;
-        const year = +d3.select("#year-slider").property("value");
+        const year = +d3.select("#year-select").property("value");
         update(year);
     });
 
-    d3.select("#play-pause-btn").on("click", () => {
-        isPaused = !isPaused;
-        d3.select("#play-pause-btn").text(isPaused ? "Play" : "Pause");
-    });
-
-    d3.select("#year-slider").on("input", function () {
-        const year = +this.value;
-        d3.select("#year-display").text(year);
-        update(year);
-    });
-
-    d3.interval(() => {
-        if (isPaused) return;
-        yearIndex = (yearIndex + 1) % years.length;
-        const year = years[yearIndex];
-        d3.select("#year-slider").property("value", year);
-        d3.select("#year-display").text(year);
-        update(year);
-    }, 1000);
-
-    update(minYear);
+    // Year filter change handler
+    d3.select("#year-select").on("change", function () {
+    const year = +this.value;  
+    d3.select("#year-display").text(year); 
+    yearIndex = years.indexOf(year);  
+    update(year);  
+    isPaused = true;  
 });
 
+// Year change interval for animation
+d3.interval(() => {
+    if (isPaused) return; 
+    yearIndex = (yearIndex + 1) % years.length; 
+    const year = years[yearIndex]; 
+    d3.select("#year-display").text(year);  
+    update(year);  
+}, 1000);
+
+// Initial update for the min year
+update(minYear);
+});
